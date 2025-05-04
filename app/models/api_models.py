a9e4a59d29178c39767d3748abd8802723a254ac
stiven106 <brayan106-@hotmail.com>
2025-05-04 10:13:26 -0500

    add: fix project structure

--- app/models/api_models.py
+++
@@ -1,7 +0,0 @@
-from flask_restx import fields
-from app.utils.api import api
-
-auth_model = api.model('Auth', {
-    "username": fields.String,
-    "password": fields.String
-})
--- app/models/hello.py
+++
@@ -1,6 +0,0 @@
-from flask_restx import fields
-from app.utils.api import api
-
-hello_model = api.model('Hello', {
-    "name": fields.String,
-})
